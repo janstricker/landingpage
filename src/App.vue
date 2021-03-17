1248cd5d6807ce1a463b3ac8ac39fb09739e8557
Jan Stricker <kontakt@janstricker.de>
2021-03-17 14:00:42 +0100

    created dummy content

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" class="mx-auto"/>
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" class="mx-auto"/>
+  <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+
+  <div class="container mx-auto flex flex-row gap-6 bg-gray-50 p-6 my-0">
+    <div class="flex-grow flex flex-row bg-white h-auto rounded-xl shadow-sm hover:shadow-xl transition-all ">
+      <div class=" w-64 bg-cover">
+        <img src="./assets/img.jpg" alt="">
+      </div>
+      <div class="p-6 w-full flex-grow flex flex-col text-left">
+        <p class=" text-xl font-bold text-gray-900 mb-4 ">
+          Title
+        </p>
+        <p class=" text-base font-normal text-gray-500 mb-4">
+          Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
+        </p>
+        <button class="px-8 py-4 rounded-full text-white font-bold bg-blue-500 hover:bg-blue-800 transition-all">Label</button>
+      </div>
+    </div>
+    <div class="flex-grow flex flex-row bg-white h-auto rounded-xl shadow-sm hover:shadow-xl transition-all ">
+      <div class=" w-64 bg-cover">
+        <img src="./assets/img.jpg" alt="">
+      </div>
+      <div class="p-6 w-full flex-grow flex flex-col text-left">
+        <p class=" text-xl font-bold text-gray-900 mb-4 ">
+          Title
+        </p>
+        <p class=" text-base font-normal text-gray-500 mb-4">
+          Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
+        </p>
+        <button class="px-8 py-4 rounded-full text-white font-bold bg-blue-500 hover:bg-blue-800 transition-all">Label</button>
+      </div>
+    </div>
+    <div class="flex-grow flex flex-row bg-white h-auto rounded-xl shadow-sm hover:shadow-xl transition-all ">
+      <div class=" w-64 bg-cover">
+        <img src="./assets/img.jpg" alt="">
+      </div>
+      <div class="p-6 w-full flex-grow flex flex-col text-left">
+        <p class=" text-xl font-bold text-gray-900 mb-4 ">
+          Title
+        </p>
+        <p class=" text-base font-normal text-gray-500 mb-4">
+          Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
+        </p>
+        <button class="px-8 py-4 rounded-full text-white font-bold bg-blue-500 hover:bg-blue-800 transition-all">Label</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <template>
-  <h1 class="text-3xl text-yellow-500 font-bold">{{ msg }}</h1>
+  <h1 class="text-3xl text-red-500 font-bold">{{ msg }}</h1>
 
   <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
-  <button @click="state.count++">count is: {{ state.count }}</button>
+  <button class="px-8 py-4 bg-black text-yellow-300 rounded-full hover:bg-gray-600 " @click="state.count++">count is: {{ state.count }}</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
